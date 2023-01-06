bff1df7f25a58bbadd5c1d754264f01210852f12
Harry-KNIGHT <eliott.knight@icloud.com>
2023-01-06 20:25:25 +0100

    Functions for add distances.

--- SCA/ViewModel/AddDistanceViewModel.swift
+++ SCA/ViewModel/AddDistanceViewModel.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 class AddDistanceViewModel: ObservableObject {
-	
+	@Published var distancesDone = Array<Distance>()
+
+	func addCounterDistance(from firstValue: Int?, to finalValue: Int?)  {
+		guard let firstValue = firstValue, let finalValue = finalValue else { return }
+
+		let counterDistance = Distance(counterStartKilometers: firstValue, counterEndKilometers: finalValue, kilometers: nil, kilometerOrCounter: .counter)
+
+		self.distancesDone.append(counterDistance)
+	}
+
+	func addDistance(kilometers: Double?) {
+		guard let kilometers = kilometers else { return }
+
+		let kilometerDistance = Distance(counterStartKilometers: nil, counterEndKilometers: nil, kilometers: kilometers, kilometerOrCounter: .distance)
+
+		self.distancesDone.append(kilometerDistance)
+	}
 }
 
